d042a9833a95e22bd669f9687d5c5a6cab3715fa
Bryan18201 <bryann1820@gmail.com>
2021-07-01 00:53:33 -0400

    Agregar archivos de proyecto.

---
+++ plll/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace plll.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
